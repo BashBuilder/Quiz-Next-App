712d6d46fd5edf998331e078a2c9b290c4ead6d8
BashBuilder <iamadelowotan@gmail.com>
2024-01-19 12:38:21 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import SetupForm from "@/components/SetupForm";
 import SelectSubject from "@/components/SubjectCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
-      <SelectSubject />
+      {/* <SelectSubject /> */}
+      <SetupForm />
     </main>
   );
 }
---
+++ components/SetupForm.tsx
@@ -0,0 +1,3 @@
+export default function SetupForm() {
+  return <div>setup form</div>;
+}
