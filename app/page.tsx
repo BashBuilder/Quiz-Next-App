9cb2e49ef141907b5970d59fb087435fb5ee5111
BashBuilder <iamadelowotan@gmail.com>
2024-01-19 14:21:54 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      {/* <SelectSubject /> */}
+      <SelectSubject />
       <SetupForm />
     </main>
   );
--- components/SetupForm.tsx
+++ components/SetupForm.tsx
@@ -1,3 +1,59 @@
 export default function SetupForm() {
-  return <div>setup form</div>;
+  return (
+    <section className="flex min-h-screen items-center justify-center py-10 md:py-20 ">
+      <form className="flex w-4/5 max-w-sm flex-col gap-4 rounded-xl bg-background px-4 md:p-10 ">
+        <h2>Quiz</h2>
+        {/* Subject */}
+        <div className="flex flex-col gap-2 ">
+          <label htmlFor="">Subject</label>
+          <select
+            name="Subject"
+            id="subject"
+            className="rounded-md bg-backgroundMain p-2 "
+          >
+            <option value="physics">Physics</option>
+            <option value="chemistry">Chemistry</option>
+            <option value="biology">Biology</option>
+          </select>
+        </div>
+        {/* Amount */}
+        <div className="flex flex-col gap-2 ">
+          <label htmlFor=""> Number of questions</label>
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            className="rounded-md bg-backgroundMain p-2 "
+            min={40}
+            max={50}
+          />
+        </div>
+
+        {/* Year of examination */}
+        <div className="flex flex-col gap-2 ">
+          <label htmlFor="year">Year</label>
+          <select
+            name="year"
+            id="year"
+            className="rounded-md bg-backgroundMain p-2 "
+          >
+            <option value="2022 ">2022</option>
+            <option value="2022">2021</option>
+            <option value="2022">2020</option>
+            <option value="2022">2019</option>
+            <option value="2022">2018</option>
+          </select>
+        </div>
+        {/* {
+          <p>Can't generate questions, please try again later</p>
+        } */}
+        <button
+          type="submit"
+          className="rounded-lg bg-primary px-4 py-2 text-background "
+        >
+          Start
+        </button>
+      </form>
+    </section>
+  );
 }
