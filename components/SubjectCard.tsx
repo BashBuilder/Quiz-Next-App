8e06a98c4db523ca1483ae57981a1aeccb108d73
BashBuilder <iamadelowotan@gmail.com>
2024-01-21 16:27:43 +0100

    update

--- components/SubjectCard.tsx
+++ components/SubjectCard.tsx
@@ -1,29 +1,30 @@
 "use client";
 import { Loader2Icon } from "lucide-react";
 import { useEffect, useState } from "react";
+
 export default function SelectSubject() {
   const [questions, setquestions] = useState([]);
+  const [questionIndex, setQuestionIndex] = useState(1);
 
   async function getQuestions() {
-    fetch("https://questions.aloc.com.ng/api/v2/m/1?subject=chemistry", {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        AccessToken: "ALOC-caa562dfeb1a7de83a69",
-      },
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log("data", data);
-        if (data) {
-          // @ts-ignore
-          questions.push({ ...data.data[0], subject: data.subject });
-        }
-      })
-      .catch((error) => {
-        console.log("error", error);
-      });
+    try {
+      const response = await fetch(
+        "https://questions.aloc.com.ng/api/v2/m/1?subject=chemistry",
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            AccessToken: "ALOC-caa562dfeb1a7de83a69",
+          },
+          method: "GET",
+        },
+      );
+      const data = await response.json();
+      // @ts-ignore
+      questions.push({ ...data.data[0], subject: data.subject });
+    } catch (error) {
+      console.log("The error from fetching is ", error);
+    }
   }
   useEffect(() => {
     getQuestions();
@@ -39,16 +40,16 @@ export default function SelectSubject() {
     return (
       <section className="flex min-h-screen flex-col items-center gap-4 py-10 md:py-20 ">
         {/* the subject panel */}
+        {/* <h5 className="text-right ">Time remaining : 2 min</h5> */}
         <h3 className="capitalize"> {subject} </h3>
         <div className="flex w-4/5 max-w-5xl flex-col gap-4 md:gap-10">
           {/* the top question section */}
           <div className=" flex flex-col gap-4 rounded-xl bg-background p-4 shadow-xl md:px-20 md:py-10 ">
-            {/* <h5 className="text-right ">Time remaining : 2 min</h5> */}
             <article className="flex flex-col gap-4 ">
               <div className="flex gap-1">
-                <p>1.</p>
+                <p className="text-xl">1.</p>
                 {/* @ts-ignore */}
-                <p> {question} </p>
+                <p className="text-xl"> {question} </p>
               </div>
               <div className="flex flex-col gap-2">
                 {
@@ -63,20 +64,9 @@ export default function SelectSubject() {
                       {option[key]}
                     </label>
                   ))
-                  // questions[0][0].option.map((opt, index) => (
-                  //   <label key={index} className=" cursor-pointer">
-                  //     <input
-                  //       type="radio"
-                  //       className="mr-2 cursor-pointer "
-                  //       name="Option"
-                  //     />
-                  //     {opt}
-                  //   </label>
-                  // ))
                 }
               </div>
             </article>
-
             {/* the next and previos button goes here */}
             <div className="my-6 flex justify-between">
               <button className="rounded-md bg-slate-200 px-4 py-2 font-bold text-slate-700 hover:shadow-md">
---
+++ components/quest.js
@@ -0,0 +1,19 @@
+// fetch("https://questions.aloc.com.ng/api/v2/m/1?subject=chemistry", {
+//   headers: {
+//     Accept: "application/json",
+//     "Content-Type": "application/json",
+//     AccessToken: "ALOC-caa562dfeb1a7de83a69",
+//   },
+//   method: "GET",
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // console.log("data", data);
+//     if (data) {
+//       // @ts-ignore
+//       questions.push({ ...data.data[0], subject: data.subject });
+//     }
+//   })
+//   .catch((error) => {
+//     console.log("error", error);
+//   });
