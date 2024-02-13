498521283921c594b8e5ec428585ff38e7378db4
BashBuilder <iamadelowotan@gmail.com>
2024-02-13 10:42:04 +0100

    Exam calculator update

--- components/CounterDownTimer.tsx
+++ components/CounterDownTimer.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { Rootstate } from "@/app/GlobalRedux/store";
+import { Calculator } from "lucide-react";
 import { useSelector } from "react-redux";
+import { Button } from "./ui/button";
+import ExamCalculator from "./ExamCalculator";
 
 export default function CounterDownTimer() {
   const timer = useSelector((state: Rootstate) => state.timer);
@@ -9,6 +12,11 @@ export default function CounterDownTimer() {
 
   return (
     <article className=" absolute right-4 top-20 mb-4  flex items-center justify-end gap-1 md:right-[12%]">
+      <ExamCalculator />
+
+      <Button className="mr-5" variant="outline">
+        <Calculator />
+      </Button>
       <div className="flex h-8 w-8 items-center justify-center rounded-md bg-primary text-white">
         <h6>{hours}</h6>
       </div>
---
+++ components/ExamCalculator.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./ui/button";
+
+export default function ExamCalculator() {
+  const [result, setResult] = useState<string>("");
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const content = e.currentTarget.textContent;
+    content && setResult(result.concat(content));
+  };
+  const clear = () => setResult("");
+  const deleteEl = () => setResult(result.slice(0, -1));
+  const calculateResult = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  return (
+    <div className="absolute top-10 m-2 flex w-60 flex-col gap-2 overflow-hidden rounded-xl bg-slate-700 p-2">
+      <input
+        type="text"
+        value={result}
+        disabled
+        className="w-full bg-transparent py-8 pr-2 text-right text-xl text-white"
+      />
+      <div className="grid grid-cols-4 gap-2">
+        <Button onClick={clear} variant="secondary">
+          AC
+        </Button>
+        <Button onClick={deleteEl} variant="secondary">
+          DE
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          .
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          /
+        </Button>
+      </div>
+      <div className="grid grid-cols-4 gap-2">
+        <Button onClick={handleClick} variant="secondary">
+          7
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          8
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          9
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          +
+        </Button>
+      </div>
+      <div className="grid grid-cols-4 gap-2">
+        <Button onClick={handleClick} variant="secondary">
+          4
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          5
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          6
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          -
+        </Button>
+      </div>
+      <div className="grid grid-cols-4 gap-2">
+        <Button onClick={handleClick} variant="secondary">
+          1
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          2
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          3
+        </Button>
+        <Button onClick={handleClick} variant="secondary">
+          +
+        </Button>
+      </div>
+      <div className="grid grid-cols-4 gap-2">
+        <Button onClick={handleClick} variant="secondary">
+          0
+        </Button>
+        <Button
+          onClick={calculateResult}
+          className="col-span-3"
+          variant="secondary"
+        >
+          =
+        </Button>
+      </div>
+    </div>
+  );
+}
