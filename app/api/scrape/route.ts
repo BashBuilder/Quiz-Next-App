4890e10344e731eef5f2276eea1e0fe0694e73ac
BashBuilder <iamadelowotan@gmail.com>
2024-02-17 20:25:53 +0100

    scrape update

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -1,5 +1,33 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
+import cheerio from "cheerio";
 
 export async function GET() {
-  return NextResponse.json({ success: "page loaded" });
+  try {
+    const url =
+      "https://myschool.ng/classroom/english-language?exam_type=jamb&page=1";
+    const response = await axios.get(url);
+    // console.log(response.data);
+
+    const $ = cheerio.load(response.data);
+
+    // @ts-ignore
+    const links = [];
+    $(".question-item").each((index, element) => {
+      links.push($(element).hasClass("question-desc"));
+    });
+
+    // @ts-ignore
+    console.log(links);
+
+    // const links : string[] = [];
+    // $("a").each((index, element) => {
+    //   links.push($(element).attr("href"));
+    // });
+
+    return NextResponse.json({ link: "access acquired" });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "error occured" });
+  }
 }
