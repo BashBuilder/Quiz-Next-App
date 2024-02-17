f4f70ee10789580904bfa2ad3002f150c4099bbb
BashBuilder <iamadelowotan@gmail.com>
2024-02-17 10:13:16 +0100

    api update

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  NextResponse.json({ success: "page loaded" });
+  return NextResponse.json({ success: "page loaded" });
 }
