d6c096c2961b3efd0d9d017e377c52d2c7ef137a
BashBuilder <iamadelowotan@gmail.com>
2024-02-28 21:59:21 +0100

    firebase update

---
+++ lib/config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNd7oF-JboB0Vr0b5QjDhyEJDY5IkDCzw",
+  authDomain: "quiz-505c4.firebaseapp.com",
+  projectId: "quiz-505c4",
+  storageBucket: "quiz-505c4.appspot.com",
+  messagingSenderId: "858016075794",
+  appId: "1:858016075794:web:1983903208330d4ce988cb",
+  measurementId: "G-C8ZV94TMNV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
