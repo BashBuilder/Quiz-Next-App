3294d3abb8bdc42d1d91cf5164e218979ea475b8
BashBuilder <iamadelowotan@gmail.com>
2024-03-30 19:40:31 +0100

    some changes removed

--- app/api/thirdscrape/route.ts
+++ app/api/thirdscrape/route.ts
@@ -34,8 +34,6 @@ export async function GET() {
     return NextResponse.json(sortedQuetion);
   } catch (error) {
     // console.log(error);
-
-    console.log("some changes made");
     return NextResponse.json({ error: "error occured" });
   }
 
